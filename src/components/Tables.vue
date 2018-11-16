241a76da1b67b104ca0ac63114092f1d9f020dfe
llr <757493952@qq.com>
2018-11-16 17:53:37 +0800

    添加了table.vue的界面

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -9,178 +9,108 @@
         <li><a href="#">入廊作业</a></li>
         <li class="active">数据</li>
       </ol>
-      <button class="btn btn-primary btn-lg" style="position: absolute; left: 200px; top: 40px;" data-toggle="modal" data-target="#myModal">添加</button>
-      </div>
-      <div class="page-inner">
-                      <div class="row">
-                          <div class="col-md-12">
-                  <div class="card">
 
-                      <!--Modal-->
-                      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
-                          <div class="modal-dialog">
-                              <div class="modal-content">
-                                  <div class="modal-header">
-                                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                      <h4 class="modal-title" id="myModalLabel">基本信息登记</h4>
-                                  </div>
-                                  <div class="modal-body">
-                                      <form class="form-horizontal" role="form">
-                                          <div class="form-group">
-                                              <label for="timelimit" class="col-sm-2 control-label">工期（日）</label>
-                                              <div class="col-sm-9">
-                                                  <input type="text" id="timelimit" name="timelimit" class="form-control well" placeholder="请输入工期"/>
-                                              </div>
-                                          </div>
-                                          <div class="form-group">
-                                              <label for="starttime" class="col-sm-2 control-label">开始时间</label>
-                                              <div class="col-sm-9">
-                                                  <input type="text" id="starttime" name="starttime" class="form-control well" placeholder="请输入开始时间"/>
-                                              </div>
-                                          </div>
-                                          <div class="form-group">
-                                              <label for="builder" class="col-sm-2 control-label">施工单位</label>
-                                              <div class="col-sm-9">
-                                                  <input type="text" id="builder" name="builder" class="form-control well" placeholder="请输入施工单位"/>
-                                              </div>
-                                          </div>
-                                          <div class="form-group">
-                                              <label for="arealimit" class="col-sm-2 control-label">活动范围（km）</label>
-                                              <div class="col-sm-9">
-                                                  <input type="text" id="arealimit" name="arealimit" class="form-control well" placeholder="请输入活动范围"/>
-                                              </div>
-                                          </div>
-                                      </form>
-                                  </div>
-                                  <div class="modal-footer">
-                                      <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                                      <button type="button" class="btn btn-primary">提交更改</button>
-                                  </div>
-                              </div>
-                          </div>
-
-                      </div>
-                      <div class="card-content">
-                          <div class="table-responsive">
-
-
-                  <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                      <thead>
-                      <tr>
-                          <th>序号</th>
-                          <th>工期</th>
-                          <th>时间</th>
-                          <th>施工单位</th>
-                          <th>活动范围</th>
-                          <th>操作</th>
-                      </tr>
-                      </thead>>
-
-                      <tbody>
-                      <tr>
-                          <td>1</td>
-                          <td>20</td>
-                          <td>2018-10-10</td>
-                          <td>BUPT</td>
-                          <td>2</td>
-                          <td><button data-toggle="modal" data-target="#edititem">修改</button><button>查看</button><button>删除</button><button>评价</button></td>
-                      </tr>
-                      <tr>
-                          <td>2</td>
-                          <td>30</td>
-                          <td>2018-10-15</td>
-                          <td>BUPT</td>
-                          <td>20</td>
-                          <td><button data-toggle="modal" data-target="#edititem">修改</button><button>查看</button><button>删除</button><button>评价</button></td>
-                      </tr>
-                      <tr>
-                          <td>3</td>
-                          <td>30</td>
-                          <td>2018-10-25</td>
-                          <td>BUPT</td>
-                          <td>10</td>
-                          <td><button data-toggle="modal" data-target="#edititem">修改</button><button>查看</button><button>删除</button><button>评价</button></td>
-                      </tr>
-                      <tr>
-                          <td>4</td>
-                          <td>30</td>
-                          <td>2018-10-15</td>
-                          <td>BUPT</td>
-                          <td>20</td>
-                          <td><button data-toggle="modal" data-target="#edititem">修改</button><button>查看</button><button>删除</button><button>评价</button></td>
-                      </tr>
-                      <tr>
-                          <td>5</td>
-                          <td>30</td>
-                          <td>2018-10-15</td>
-                          <td>BUPT</td>
-                          <td>20</td>
-                          <td><button data-toggle="modal" data-target="#edititem">修改</button><button>查看</button><button>删除</button><button>评价</button></td>
-                      </tr>
-                      </tbody>
-
-
-                  </table>
-                              <!--Modal-->
-                              <div class="modal fade" id="edititem" tabindex="-1" role="dialog" aria-labelledby="edititem" aria-hidden="true">
-                                  <div class="modal-dialog">
-                                      <div class="modal-content">
-                                          <div class="modal-header">
-                                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                              <h4 class="modal-title" id="edititemLabel">基本信息登记</h4>
-                                          </div>
-                                          <div class="modal-body">
-                                              <form class="form-horizontal" role="form">
-                                                  <div class="form-group">
-                                                      <label for="edittimelimit" class="col-sm-2 control-label">工期（日）</label>
-                                                      <div class="col-sm-9">
-                                                          <input type="text" id="edittimelimit" name="edittimelimit" class="form-control well" placeholder="请输入工期"/>
-                                                      </div>
-                                                  </div>
-                                                  <div class="form-group">
-                                                      <label for="editstarttime" class="col-sm-2 control-label">开始时间</label>
-                                                      <div class="col-sm-9">
-                                                          <input type="text" id="editstarttime" name="editstarttime" class="form-control well" placeholder="请输入开始时间"/>
-                                                      </div>
-                                                  </div>
-                                                  <div class="form-group">
-                                                      <label for="editbuilder" class="col-sm-2 control-label">施工单位</label>
-                                                      <div class="col-sm-9">
-                                                          <input type="text" id="editbuilder" name="editbuilder" class="form-control well" placeholder="请输入施工单位"/>
-                                                      </div>
-                                                  </div>
-                                                  <div class="form-group">
-                                                      <label for="editarealimit" class="col-sm-2 control-label">活动范围（km）</label>
-                                                      <div class="col-sm-9">
-                                                          <input type="text" id="editarealimit" name="editarealimit" class="form-control well" placeholder="请输入活动范围"/>
-                                                      </div>
-                                                  </div>
-                                              </form>
-                                          </div>
-                                          <div class="modal-footer">
-                                              <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                                              <button type="button" class="btn btn-primary">提交更改</button>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      </div>
-                          </div>
-                      </div>
       </div>
 
-
-
+  <div id="pageBody">
+    <el-table
+      :data="tableData.filter(data=>!search||data.name.toLowerCase().includes(search.toLowerCase()))"
+    style="width:98%;left: 20px;">
+      <el-table-column
+      label="序号"
+      prop="id">
+      </el-table-column>
+      <el-table-column
+        label="工期"
+        prop="duration">
+      </el-table-column>
+      <el-table-column
+        label="时间"
+        prop="date">
+      </el-table-column>
+      <el-table-column
+        label="施工人员数量"
+        prop="work_number">
+      </el-table-column>
+      <el-table-column
+        label="活动范围"
+        prop="activity_range">
+      </el-table-column>
+      <el-table-column
+        label="评价"
+        prop="evaluation">
+      </el-table-column>
+      <el-table-column
+        width="300"
+        align="right">
+        <template slot="header" slot-scope="slot">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="Type to search"/>
+        </template>
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            @click="handleCheck(scope.$index, scope.row)">查看</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          <el-button
+            size="mini"
+            @click="handleEvaluate(scope.$index, scope.row)">评价</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
+
+</div>
 
 </template>
 
 <script>
 
     export default {
-        name: "Tables"
+      data() {
+        return {
+          tableData: [],
+          getUrl: 'http://10.112.17.185:8100/api/v1/info/entranceWork',
+          search: ''
+        }
+      },
+      created: function () {
+        this.getTableData()
+      },
+      methods: {
+        getTableData: function () {
+          var vm = this
+          //var self = this.tableData;
+          $.ajax({
+            type: "GET",
+            dataType: "JSON",
+            //  header:"Access-Control-Allow-Origin:  http://10.112.17.185:8100",
+            url: "http://10.112.17.185:8100/api/v1/info/entranceWork",
+            success: function (msg) {
+              vm.tableData = msg;
+            },
+            error: function (err) {
+              alert("加载0000失败");
+            }
+          });
+        },
+        handleEdit() {
+        },
+        handleCheck(){
+
+        },
+        handleDelete() {
+        },
+        handleEvaluate(){}
+      }
     }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ import $ from 'jquery'
 import 'assets/js/bootstrap.min.js'
 //import './static/css/bootstrap-theme.min.css'
 import router from './router/router.js'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
-Vue.use(ElementUI);
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
